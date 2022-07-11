d056d9db086500fd33ef49b081658fd8828fa51f
Jeremy Christian <jeremy.l.christian@gmail.com>
2022-07-11 11:47:35 -0700

    Finalized RPS script. Made game responsive for tablets and phones

--- script.js
+++ script.js
@@ -3,47 +3,47 @@ const paper = document.querySelector('#paper');
 const scissors = document.querySelector('#scissors');
 
 const playText = document.querySelector('.gametext');
+const currentScore = document.querySelector('.scoretext');
 
 let playerPoints = 0;
 let computerPoints = 0;
-
-// function restartGame() {
-//     rock.disabled = false;
-//     paper.disabled = false;
-//     scissors.disabled = false;
-//     let playerPoints = 0;
-//     let computerPoints = 0;
-
-//     document.body.removeChild(playAgain);
-// }
 
 function computerPlay() {
     let computerChoice = Math.floor(Math.random() * 3);
 
     if (computerChoice === 0 && this.id === 'rock') {
-        playText.textContent = `You chose ${this.id} and computer chose Rock. Tie! Current score: Player: ${playerPoints} Computer: ${computerPoints}`;
+        playText.textContent = `You chose ${this.id} and computer chose Rock. Tie!`;
+        currentScore.textContent = `Current score - Player: ${playerPoints} Computer: ${computerPoints}`;
     } else if (computerChoice === 0 && this.id === 'paper') {
         playerPoints += 1;
-        playText.textContent = `You chose ${this.id} and computer chose Rock. Win! Current score: Player: ${playerPoints} Computer: ${computerPoints}`;
+        playText.textContent = `You chose ${this.id} and computer chose Rock. Win!`;
+        currentScore.textContent = `Current score - Player: ${playerPoints} Computer: ${computerPoints}`;
     } else if (computerChoice === 0 && this.id === 'scissors') {
         computerPoints += 1;
-        playText.textContent = `You chose ${this.id} and computer chose Rock. Lose! Current score: Player: ${playerPoints} Computer: ${computerPoints}`;
+        playText.textContent = `You chose ${this.id} and computer chose Rock. Lose!`;
+        currentScore.textContent = `Current score - Player: ${playerPoints} Computer: ${computerPoints}`;
     } else if (computerChoice === 1 && this.id === 'rock') {
         computerPoints += 1;
-        playText.textContent = `You chose ${this.id} and computer chose Paper. Lose! Current score: Player: ${playerPoints} Computer: ${computerPoints}`;
+        playText.textContent = `You chose ${this.id} and computer chose Paper. Lose!`;
+        currentScore.textContent = `Current score - Player: ${playerPoints} Computer: ${computerPoints}`;
     } else if (computerChoice === 1 && this.id === 'paper') {
-        playText.textContent = `You chose ${this.id} and computer chose Paper. Tie! Current score: Player: ${playerPoints} Computer: ${computerPoints}`;
+        playText.textContent = `You chose ${this.id} and computer chose Paper. Tie!`;
+        currentScore.textContent = `Current score - Player: ${playerPoints} Computer: ${computerPoints}`;
     } else if (computerChoice === 1 && this.id === 'scissors') {
         playerPoints += 1;
-        playText.textContent = `You chose ${this.id} and computer chose Paper. Win! Current score: Player: ${playerPoints} Computer: ${computerPoints}`;
+        playText.textContent = `You chose ${this.id} and computer chose Paper. Win!`;
+        currentScore.textContent = `Current score - Player: ${playerPoints} Computer: ${computerPoints}`;
     } else if (computerChoice === 2 && this.id === 'rock') {
         playerPoints += 1;
-        playText.textContent = `You chose ${this.id} and computer chose Scissors. Win! Current score: Player: ${playerPoints} Computer: ${computerPoints}`;
+        playText.textContent = `You chose ${this.id} and computer chose Scissors. Win!`;
+        currentScore.textContent = `Current score - Player: ${playerPoints} Computer: ${computerPoints}`;
     } else if (computerChoice === 2 && this.id === 'paper') {
         computerPoints += 1;
-        playText.textContent = `You chose ${this.id} and computer chose Scissors. Lose! Current score: Player: ${playerPoints} Computer: ${computerPoints}`;
+        playText.textContent = `You chose ${this.id} and computer chose Scissors. Lose!`;
+        currentScore.textContent = `Current score - Player: ${playerPoints} Computer: ${computerPoints}`;
     } else if (computerChoice === 2 && this.id === 'scissors') {
-        playText.textContent = `You chose ${this.id} and computer chose Scissors. Tie! Current score: Player: ${playerPoints} Computer: ${computerPoints}`;
+        playText.textContent = `You chose ${this.id} and computer chose Scissors. Tie!`;
+        currentScore.textContent = `Current score - Player: ${playerPoints} Computer: ${computerPoints}`;
     } else {
         console.log('Broken, check code');
     }
@@ -52,17 +52,25 @@ function computerPlay() {
         rock.disabled = true;
         paper.disabled = true;
         scissors.disabled = true;
+        rock.classList.remove('hover');
+        paper.classList.remove('hover');
+        scissors.classList.remove('hover');
         
         let playAgain = document.createElement('button');
         playAgain.textContent = 'Play Again';
+        playAgain.classList.add('playagainbutton')
         document.body.appendChild(playAgain);
         playAgain.addEventListener('click', () => {
             rock.disabled = false;
             paper.disabled = false;
             scissors.disabled = false;
+            rock.classList.add('hover');
+            paper.classList.add('hover');
+            scissors.classList.add('hover');
             playerPoints -= playerPoints;
             computerPoints -= computerPoints;
             playText.textContent = ``;
+            currentScore.textContent = ``;
             document.body.removeChild(playAgain);
         });
     } else {
